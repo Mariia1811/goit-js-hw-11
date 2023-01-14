40c74d5a975cae9394a03692d64d3d7e807fde48
Mariia1811 <115475304+Mariia1811@users.noreply.github.com>
2023-01-14 23:01:14 +0200

    rework

--- src/index.js
+++ src/index.js
@@ -17,47 +17,46 @@ let page = 1;
 let searchQuery = '';
 refs.btnEl.style.visibility = 'hidden';
 
+async function creatrGallery() {
+  const response = await getImg(searchQuery, page);
+  createListMarkup(response.data.hits);
+  allPage = response.data.totalHits / 40;
+  return response;
+}
+
 async function onFormSubmit(e) {
   e.preventDefault();
 
   searchQuery = e.currentTarget.elements.searchQuery.value.trim();
   page = 1;
   refs.divEl.innerHTML = '';
+
   if (!searchQuery) {
     refs.btnEl.style.visibility = 'hidden';
     return;
   }
-  try {
-    const response = await getImg(searchQuery, page);
-    allPage = response.data.totalHits / 40;
 
-    if (response.data.total !== 0) {
-      Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
-      createListMarkup(response.data.hits);
+  try {
+    const gallery = await creatrGallery();
+    if (gallery.data.total !== 0) {
+      Notify.info(`Hooray! We found ${gallery.data.totalHits} images.`);
     } else {
       throw new Error();
     }
-  } catch (error) {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-  if (page <= allPage) {
-    refs.btnEl.style.visibility = 'visible';
-  }
-}
-
-refs.btnEl.addEventListener('click', async () => {
-  page += 1;
-
-  try {
-    const response = await getImg(searchQuery, page);
-    createListMarkup(response.data.hits);
   } catch {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
+
+  if (page <= allPage) {
+    refs.btnEl.style.visibility = 'visible';
+  }
+}
+
+refs.btnEl.addEventListener('click', () => {
+  page += 1;
+  creatrGallery();
 
   if (page >= allPage) {
     Notify.success(
@@ -102,6 +101,7 @@ function createListMarkup(img) {
   lightbox.refresh();
   scroll();
 }
+
 let lightbox = new SimpleLightbox('.gallery a');
 
 function scroll() {
